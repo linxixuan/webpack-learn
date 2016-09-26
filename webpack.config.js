d58771d35cf1e7360595c5ae2f6d2ebb259138e6
greennn <zanbizhnyi@vip.qq.com>
2016-09-26 22:57:26 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+// webpack.config.js
+var path = require('path')
+var webpack = require('webpack');
+require('./style.css');
+require('./demo.png');
+
+module.exports = {
+  entry: ['./src/index','./style.css','./demo.png'],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+      },
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.css$/,
+      loaders: ['style', 'css']
+    },
+    {
+        test: /\.(png|jpg)$/,
+        loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+    }]
+  }
+}
